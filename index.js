d4ada248f89b5094cfed8d099b745b8e8e02c519
naranjaFanta <eduardo.tecmet@hotmail.com>
2024-07-01 19:02:08 -0300

    preparamos carpetas scr y mvc

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+import express from 'express';
+
+
+const app = express();
+const PORT = 8080;
+
+// Middleware para parsear JSON y datos de URL
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Inicialización del servidor en el puerto especificado
+app.listen(PORT, () => console.log(`Servidor listo en puerto ${PORT}`));
+
+// Manejo de errores del servidor
+app.on("error", (error) => console.log(`Error !!!!!!!!: ${error}`));
